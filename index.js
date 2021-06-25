dd1ff16797de1d1e33c41a86d5a745a68daa1a06
olesya <olesyam.miller@gmail.com>
2021-06-25 09:52:33 -0400

    updated the index file

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const prompt = require('prompt-sync')({sigint: true});
 class BookSearch {
 
     constructor() {
-      this.myBooks = []
+        this.myBooks = []
+        this.counter = 0
     }
 
     baseUrl = "https://www.googleapis.com/books/v1/volumes?q="
@@ -14,14 +15,14 @@ class BookSearch {
         fetch(this.baseUrl+`${query}&projection=lite&key=${this.apiKey}`)
             .then(res => res.json())
             .then(data => {
-             this.filterData(data)
-        })
+                this.filterData(data)
+            })
     }
 
     filterData(data){
         const filteredData = data.items.filter(volume => volume.volumeInfo.publisher && volume.volumeInfo.authors).slice(0,5)
         filteredData.forEach((volume, index) => this.displayVolumeData(index, volume.volumeInfo))
-        this.addBook(filteredData)
+        this.handleInput(filteredData)
     }
 
     displayVolumeData(ind, volume) {
@@ -33,99 +34,82 @@ class BookSearch {
         console.log("")
     }
 
-    interactionWithUser(helperArg){
-        console.log(`To add ${helperArg} book to your reading list enter the number of the book`)
-        console.log("To see your reading list type 'list'")
-        console.log("To start a new search type 'search'")
+    addBook(data, input) {
+        this.myBooks.push(data[Number(input) - 1])
+        console.log("THE BOOK HAS BEEN ADDED TO YOUR READING LIST!")
+        this.handleInput(data)
     }
 
-    addBook(data, helperArg = ''){
+    handleInput(data){
         console.log("")
-        this.interactionWithUser(helperArg)
+        this.interactionWithUser()
         let input = prompt("")
-        input = input.toLowerCase()
         console.log("")
-        this.validateAndAddBook(data, input)
-        if (input === "list") {
-            console.log("")
-            console.log("YOUR READING LIST:")
-            this.myBooks.length === 0 ? console.log("Your reading list is empty") : this.myBooks.forEach((book, ind) => this.displayVolumeData(ind, book.volumeInfo))
-            this.addBook(data, "another")
+        input = input.toLowerCase()
+        if(Number(input) >= 1 && Number(input) <= data.length){
+            this.addBook(data, input)
+        } else if (input === "list") {
+            this.displayReadingList()
+            this.handleInput(data)
         } else if (input === "search") {
-            this.handleSearchInput()
+            this.start()
         } else if (input === "exit") {
             return
         } else if (Number(input) >= data.length) {
             console.log(`The book number ${Number(input)} doesn't exist. Please enter correct book number`)
-            this.handleCorrectBookNumberInput(data)
+            this.handleInput(data)
         } else {
-            console.log("PLEASE ENTER CORRECT INPUT inside addBook")
-            this.addBook(data, "another")
+            console.log("PLEASE ENTER CORRECT INPUT")
+            this.handleInput(data)
         }
-      }
+    }
 
-      validateAndAddBook(data, input){
-          if(Number(input) >= 1 && Number(input) <= data.length){
-              this.myBooks.push(data[Number(input) - 1])
-              console.log("The book has been successfully added to your collection!")
-              this.addBook(data, "another")
-          }
-      }
+    displayReadingList() {
+        console.log("")
+        console.log("YOUR READING LIST:")
+        this.myBooks.length === 0 ? console.log("Your reading list is empty") : this.myBooks.forEach((book, ind) => this.displayVolumeData(ind, book.volumeInfo))
+    }
 
-      handleCorrectBookNumberInput(data) {
-          let input = prompt("")
-          console.log("")
-          // if(Number(input) >= 1 && Number(input) <= data.length){
-          //     this.myBooks.push(data[Number(input) - 1])
-          //     console.log("The book has been successfully added to your collection!")
-          //     this.addBook(data,"another")
-          // }
-          this.validateAndAddBook(data, input)
-      }
-
-      handleSearchInput(){
-          this.greetingHelper()
-          let input = prompt('')
-          console.log("")
-          if(input === "exit") {
-              return
-          }
-          else if (input.trim() === "" || Number(input)) {
-              console.log("PLEASE ENTER CORRECT INPUT")
-              this.handleSearchInput()
-          }
-          else {
-              this.getBooks(input)
-          }
-      }
-
-      greeting(){
-          console.log("Welcome to the Book Finder app!")
-          console.log("To exit the program at any time type 'exit'")
-          this.greetingHelper()
-      }
-
-      greetingHelper() {
-          console.log('Enter the title or the author name of the book')
-      }
-
-      start(){
-        this.greeting()
-        let input = prompt('')
+    start(){
+        this.counter++
+        this.counter > 1 ? this.searchPrompt() : this.greeting()
+        const input = prompt('')
         console.log("")
         if(input === "exit") {
             return
-        } 
-        else if (input.trim() === "" || Number(input)) {
+        }
+        else if (input.trim() === "" || Number(input) || "[@_!#$%^&*()<>?/|}{~:`-+=.,']".includes(input) || '"'.includes(input) || this.checkInputForSymbolsComb(input)) {
             console.log("PLEASE ENTER CORRECT INPUT")
             this.start()
-        } 
+        }
         else {
             this.getBooks(input)
         }
     }
-      
-}
 
-const newSearch = new BookSearch([])
+    checkInputForSymbolsComb(input) {
+        console.log(input.split(""))
+
+        input.split("").forEach(symbol => {
+            "[@_!#$%^&*()<>?/|}{~:`-+=.,']".includes(symbol) ? false : true
+        })
+    }
+
+    interactionWithUser(){
+        console.log(`To add book to your reading list enter the number of the book`)
+        console.log("To see your reading list type 'list'")
+        console.log("To start a new search type 'search'")
+    }
+
+    searchPrompt() {
+        console.log('Enter the title or the author of the book')
+    }
+
+    greeting(){
+        console.log("Welcome to the Book Finder app!")
+        console.log("To exit the program at any time type 'exit'")
+        this.searchPrompt()
+    }
+}
+const newSearch = new BookSearch()
 newSearch.start()
