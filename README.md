2d21ffa8c6cd75392d8c79ee51fbf35c1b1f8a9f
olesya <olesyam.miller@gmail.com>
2021-06-25 11:22:03 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+8th-light-assessment-cli
+Welcome to my JavaScript CLI Google Books API search app!
+
+My application lets the user search for their favorite books and save them to the reading list.
+
+For the app to work first run 'npm install' command to install necessary packages.
+
+To start the app simply type 'node index.js' in the terminal.
+
+I hope you have the best experience with my product!
---
+++ index.test.js
@@ -0,0 +1,51 @@
+import {BookSearch} from "./index";
+const inst = new BookSearch()
+
+let returnedData
+fetch(this.baseUrl+`${query}&projection=lite&key=${this.apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+        returnedData = data
+    })
+
+describe('#start', function() {
+    it( "should call helper methods",function() {
+        inst.start()
+        expect(inst.greeting).toHaveBeenCalled()
+    });
+    it("validates user input", function () {
+        let input = "book"
+        let symbol = Symbol()
+        expect(input).toBeNaN()
+        expect(input).not.toBeInstanceOf(Symbol)
+        expect(input).not.toEqual(" ")
+        expect(input).not.toContain(symbol)
+    });
+});
+
+describe("#filterData", function () {
+    it("should filter returned data")
+    const filteredData = inst.filterData(returnedData)
+    filteredData.forEach(book => {
+        expect(book).toHaveProperty("title")
+        expect(book).toHaveProperty("authors")
+        expect(book).toHaveProperty("publisher")
+        expect(inst.handleInput()).toHaveBeenCalled()
+    })
+
+})
+
+describe("getBooks", function () {
+    it("should fetch data from Google Books API", async () => {
+        let query = "Harry"
+        expect(inst.getBooks(query))
+        await request()
+            .get(`/googleapis.com/books/v1/volumes?q=${query}&projection=lite&key=AIzaSyAAOCTRfoHkv8KW7h3BjVvIe-z_NIZYgig`)
+            .expect(200)
+            .then((res) => {
+                res.items.forEach(item => {
+                    expect(item.title || item.author).toContain(query);
+                })
+            })
+    })
+})
